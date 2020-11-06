7ff91816498ef39c7f4b350ea2c862e99b018693
Hashem <hashem.sf@gmail.com>
2020-11-06 19:54:33 +0300

    added header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import React from "react";
 import "./App.css";
-import HomePage from "./pages/homepage/HomePage.component";
 import { Switch, Route } from "react-router-dom";
+
+import HomePage from "./pages/homepage/HomePage.component";
 import ShopPage from "./pages/shop/ShopPage.componenet";
+import Header from "./components/header/Header.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/">
           <HomePage />
